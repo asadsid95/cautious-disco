ffe65af82b2a6d3d0793f2e6c2aa594cdce3e851
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-08-17 14:31:27 -0400

    thread card + main feed with all threads added + post detail page (i.e. clicking on a post) added

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -37,3 +37,82 @@ export async function createThread({ text, author, path, communityId }: Params) 
     }
 
 }
+
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+
+    connectToDB()
+
+    //calculate number of posts to skip
+    const skipAmount = (pageNumber - 1) * pageSize
+
+    // Fetchs posts that have no parents (i.e. top level threads )
+    const postsQuery = Thread.find({
+        parentId: { $in: [null, undefined] }
+    })
+        .sort({ createdAt: 'desc' })
+        .skip(skipAmount)
+        .limit(pageSize)
+        .populate({ path: 'author', model: User })
+        .populate({
+            path: 'children',
+            populate: {
+                path: 'author',
+                model: User,
+                select: '_id name parentId image'
+            }
+        })
+
+    const totalPostsCount = await Thread.countDocuments({
+        parentId: {
+            $in: [null, undefined]
+        }
+    })
+
+    const posts = await postsQuery.exec()
+    const inNext = totalPostsCount > skipAmount + posts.length
+
+    return { posts, inNext }
+
+}
+
+
+export async function fetchThreadById(id: string) {
+
+    connectToDB()
+
+    try {
+
+        // TODO: populate community 
+
+        // this query is fetching the parent post, its children, and the grand childrens (childrens of children)
+        const thread = await Thread.findById(id)
+            .populate({
+                path: 'author',
+                model: User,
+                select: '_id id name image'
+            })
+            .populate({
+                path: 'children',
+                populate: [{
+                    path: 'author',
+                    model: User,
+                    select: '_id id name parentId imag`e'
+                },
+                {
+                    path: 'children',
+                    model: Thread,
+                    populate: {
+                        path: 'author',
+                        model: User,
+                        select: "_id id name parentId image"
+                    }
+                }
+                ]
+            }).exec();
+        return thread
+    } catch (error) {
+
+    }
+
+}
