c5458fda666923cd16b8518bbf2991a52bdf791e
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-08-16 22:20:08 -0400

    mongodb setup + user model + user creation added

--- components/forms/AccountProfiles.tsx
+++ components/forms/AccountProfiles.tsx
@@ -20,6 +20,8 @@ import Image from "next/image";
 import { ChangeEvent, useState } from "react";
 import { isBase64Image } from "@/lib/utils";
 import { useUploadThing } from '@/lib/uploadthing'
+import { updateUser } from "@/lib/actions/user.actions";
+import { usePathname, useRouter } from "next/navigation";
 
 interface Props {
     user: {
@@ -37,6 +39,8 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 
     const [files, setFiles] = useState<File[]>([])
     const { startUpload } = useUploadThing('media')
+    const router = useRouter()
+    const pathname = usePathname()
 
     const form = useForm({
         resolver: zodResolver(UserValidation),
@@ -92,7 +96,25 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                 values.profile_photo = imgRes[0].fileUrl
             }
         }
-        // TODO: Update user profile by calling a backend function
+
+        // Update user profile by calling a backend function
+        await updateUser(
+            {
+                userId: user.id,
+                username: values.username,
+                name: values.name,
+                bio: values.bio,
+                image: values.profile_photo,
+                path: pathname
+
+            })
+
+        // this path will happen if user decides to edit their profile; thus navigate back to the page they came from or else if user is creating their profile via /onboarding, navigate forward to home
+        if (pathname == "/profile/edit") {
+            router.back();
+        } else {
+            router.push('/')
+        }
     }
 
     return (
---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,49 @@
+'use server'
+
+import mongoose from "mongoose";
+import { connectToDB } from "../mongoose"
+import User from "../models/user.model";
+import { revalidatePath } from "next/cache";
+
+interface Params {
+    userId: string
+    username: string
+    name: string
+    bio: string
+    image: string
+    path: string
+}
+
+export async function updateUser({
+    userId,
+    username,
+    name,
+    bio,
+    image,
+    path
+}: Params
+): Promise<void> {
+
+    connectToDB();
+
+    try {
+        await User.findOneAndUpdate(
+            { id: userId },
+            {
+                username: username.toLowerCase(),
+                name,
+                bio,
+                image,
+                onboarded: true
+            },
+            { upsert: true }
+        )
+
+        // if the user is editing their profile, revalide the path upon upserting
+        if (path === '/profile/edit') {
+            revalidatePath(path)
+        }
+    } catch (error) {
+        throw new Error(`Failed to create/update user: ${error.message}`)
+    }
+}
