a1cd4075fd5590fe29d85005f6cef40ffeafe2ef
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-08-18 01:07:27 -0400

    created template pages for activity, communities and search

---
+++ app/(root)/activity/page.tsx
@@ -0,0 +1,8 @@
+export default async function Page() {
+
+    return (
+        <section>
+            <h1>Activity</h1>
+        </section>
+    )
+}
---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,8 @@
+export default async function Page() {
+
+    return (
+        <section>
+            <h1>Search</h1>
+        </section>
+    )
+}
